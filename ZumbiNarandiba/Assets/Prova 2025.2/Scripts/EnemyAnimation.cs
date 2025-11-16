2e9d516c1088f1696b8199b2119e0f8a92dc682f
IsaacReisLindo <isaacreis209@gmail.com>
2025-11-16 01:38:01 -0300

    Animações Finalizadas
    Animações do zumbi 1 e player finalizada, agora temos que arrumar o dano rapido

--- ZumbiNarandiba/Assets/Prova 2025.2/Scripts/EnemyAnimation.cs
+++ ZumbiNarandiba/Assets/Prova 2025.2/Scripts/EnemyAnimation.cs
@@ -1,43 +1,41 @@
-using System.Collections;
 using UnityEngine;
+using System.Collections;
 
 public class EnemyAnimation : MonoBehaviour
 {
     private Animator animator;
 
-    public string[] idleDirections = { "N idle", "NW idle", "W idle", "SW idle", "S idle", "SE idle", "E idle", "NE idle" };
-    public string[] walkDirections = { "N walk", "NW walk", "W walk", "SW walk", "S walk", "SE walk", "E walk", "NE walk" };
-    public string[] attackDirections = { "N attack", "NW attack", "W attack", "SW attack", "S attack", "SE attack", "E attack", "NE attack" };
-    public string[] deathDirections = { "N death", "NW death", "W death", "SW death", "S death", "SE death", "E death", "NE death" };
+    public string[] idleDirections = { "N Idle", "NO Idle", "O Idle", "SO Idle", "S Idle", "SE Idle", "L Idle", "NO Idle" };
+    public string[] walkDirections = { "N Walk", "NO Walk", "O Walk", "SO Walk", "S Walk", "SE Walk", "L Walk", "NO Walk" };
+    public string[] attackDirections = { "N Atack", "NO Atack", "O Atack", "SO Atack", "S Atack", "SE Atack", "L Atack", "NO Atack" };
+    public string[] deathDirections = { "N Die", "NO Die", "O Die", "SO Die", "S Die", "SE Die", "L Die", "NO Die" };
 
-    private int lastDirection = 4;
+    private int lastDirection;
     private bool isAttacking = false;
+    public bool IsAttacking => isAttacking;
 
     private bool isDead = false;
     public bool IsDead => isDead;
-
-    public bool IsAttacking => isAttacking;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
     }
 
-    // Chamada pelo script Enemy.cs
     public void SetDirection(Vector2 direction)
     {
         if (isAttacking) return;
 
         string[] array;
 
-        if (direction.sqrMagnitude < 0.01f)
+        if (direction.magnitude < 0.1f)
         {
             array = idleDirections;
         }
         else
         {
-            lastDirection = DirectionToIndex(direction);
             array = walkDirections;
+            lastDirection = DirectionToIndex(direction);
         }
 
         animator.Play(array[lastDirection]);
@@ -56,10 +54,8 @@ public class EnemyAnimation : MonoBehaviour
 
     private IEnumerator AttackRoutine()
     {
-        yield return null;
-        float duration = animator.GetCurrentAnimatorStateInfo(0).length;
-        yield return new WaitForSeconds(duration);
-
+        float time = animator.GetCurrentAnimatorStateInfo(0).length;
+        yield return new WaitForSeconds(time);
         isAttacking = false;
     }
 
@@ -70,25 +66,20 @@ public class EnemyAnimation : MonoBehaviour
         isDead = true;
 
         animator.Play(deathDirections[lastDirection]);
-
-        StartCoroutine(FreezeAndDestroy());
+        StartCoroutine(FreezeDeathFrame());
     }
 
-    private IEnumerator FreezeAndDestroy()
+    private IEnumerator FreezeDeathFrame()
     {
         float duration = animator.GetCurrentAnimatorStateInfo(0).length;
         yield return new WaitForSeconds(duration);
 
         animator.speed = 0f;
-
-        yield return new WaitForSeconds(5f);
-
-        Destroy(gameObject);
     }
 
-    private int DirectionToIndex(Vector2 dir)
+    private int DirectionToIndex(Vector2 direction)
     {
-        Vector2 n = dir.normalized;
+        Vector2 n = direction.normalized;
 
         float step = 360f / 8f;
         float offset = step / 2f;
@@ -97,7 +88,9 @@ public class EnemyAnimation : MonoBehaviour
         angle += offset;
 
         if (angle < 0)
+        {
             angle += 360f;
+        }
 
         return Mathf.FloorToInt(angle / step);
     }
